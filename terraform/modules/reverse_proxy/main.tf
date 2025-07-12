38db68494b6b719c5ff8faa4c8a92bc9d82752f8
andriy29k <andriykorotchuk345@gmail.com>
2025-07-12 11:06:18 +0300

    Init project

---
+++ terraform/modules/reverse_proxy/main.tf
@@ -0,0 +1,23 @@
+resource "google_compute_instance" "reverse_proxy" {
+  name         = "reverse-proxy"
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+      size  = var.size
+    }
+  }
+
+  metadata = {
+    ssh-keys = "${var.ssh_user}:${file(var.ssh_path_over_bastion)}"
+  }
+
+  network_interface {
+    subnetwork = var.public_subnet_name
+    access_config {}
+  }
+
+  tags = ["reverse-proxy"]
+}
