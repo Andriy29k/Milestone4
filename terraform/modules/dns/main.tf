e4918bf07e4ebca114294bf9de8e4b40ec53cdf3
andriy29k <andriykorotchuk345@gmail.com>
2025-07-12 14:10:13 +0300

    Add gitignore

--- terraform/modules/dns/main.tf
+++ terraform/modules/dns/main.tf
@@ -11,4 +11,5 @@ resource "google_dns_record_set" "a_record" {
   ttl          = 300
   managed_zone = google_dns_managed_zone.main.name
   rrdatas      = [var.external_ip]
+  depends_on = [ google_compute_instance.reverse_proxy ]
 }
